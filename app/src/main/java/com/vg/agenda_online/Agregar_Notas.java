8c919a262820912200afd77c206040b3549777e2
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-10-31 21:06:39 -0600

    Continuidad de proyecto

---
+++ app/src/main/java/com/vg/agenda_online/Agregar_Notas.java
@@ -0,0 +1,14 @@
+package com.vg.agenda_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Agregar_Notas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_notas);
+    }
+}
