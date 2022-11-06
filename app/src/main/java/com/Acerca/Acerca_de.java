a6848d1aa6121eff0f333caffcf8f7245bd0af63
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-11-05 19:58:53 -0600

    Creacion de carpetas y botones de la vista principal

---
+++ app/src/main/java/com/Acerca/Acerca_de.java
@@ -0,0 +1,16 @@
+package com.Acerca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vg.agenda_online.R;
+
+public class Acerca_de extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
--- app/src/main/java/com/MainActivity.java
+++ app/src/main/java/com/MainActivity.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.Acerca.Acerca_de;
+import com.Contacto.Contactos;
+import com.Importantes.Importante_Notas;
+import com.ListarNotas.Listar_Notas;
+import com.vg.agenda_online.Agregar_Notas;
 import com.vg.agenda_online.R;
 
 public class MainActivity extends AppCompatActivity {
 
     Button Btn_Login, Btn_Registro;
+    Button btnagregar, btnmisnotas, btnimportantes, btncontacto, btnacerca, btnsalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
 
         Btn_Login=findViewById(R.id.Btn_Login);
         Btn_Registro=findViewById(R.id.Btn_Registro);
+        btnagregar=findViewById(R.id.btnagregar);
+        btnmisnotas=findViewById(R.id.btnmisnotas);
+        btnimportantes=findViewById(R.id.btnimportantes);
+        btncontacto=findViewById(R.id.btncontacto);
+        btnacerca=findViewById(R.id.btnacerca);
 
         Btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +43,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,Registro.class));
+            }
+        });
+
+        btnagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Agregar_Notas.class));
+            }
+        });
+        btnmisnotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Listar_Notas.class));
+            }
+        });
+        btnimportantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Importante_Notas.class));
+            }
+        });
+        btncontacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Contactos.class));
+            }
+        });
+        btnacerca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Acerca_de.class));
             }
         });
 
