d2c398123448c087791bdb9bb40e8c808da7653f
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-11-07 19:51:03 -0600

    Subiendo datos para cargar datos del usuario actual

--- app/src/main/java/com/ListarNotas/Listar_Notas.java
+++ app/src/main/java/com/ListarNotas/Listar_Notas.java
@@ -3,8 +3,6 @@ package com.ListarNotas;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.os.Bundle;
@@ -16,120 +14,8 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
 
 public class Listar_Notas extends AppCompatActivity {
 
-    RecyclerView recyclerviewNotas;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReferenceBASE_DE_DATOS;
-
-    LinearLayoutManager linearLayoutManager;
-
-    FirebaseRecyclerAdapter<Nota, ViewHolder_Nota> firebaseRecyclerAdapter;
-    FirebaseRecyclerOptions<Nota> options;
-
-    Dialog dialog;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listar_notas);
-
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.setTitle("Mis notas");
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-        recyclerviewNotas = findViewById(R.id.recyclerviewNotas);
-        recyclerviewNotas.setHasFixedSize(true);
-
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        BASE_DE_DATOS = firebaseDatabase.getReference("Notas_Publicadas");
-        dialog = new Dialog(Listar_Notas.this)
-        ListarNotasUsuarios();
-    }
-
-    private void ListarNotasUsuarios(){
-        options = new FirebaseRecyclerOptions.Builder<Nota>().setQuery(BASE_DE_DATOS, Nota.class).build();
-        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Nota, ViewHolder_Nota>(Options){
-            @Override
-            protected void onBindViewHolder(@NonNull ViewHolder_Nota viewHolder_nota, int position, @NonNull Nota nota){
-                viewHolder_nota.SetearDatos(
-                        getApplicationContext(),
-                        nota.getUid_nota(),
-                        nota.getUid_usuario(),
-                        nota.getcorreo_usuario(),
-                        nota.getFecha_horaactual(),
-                        nota.gettitle(),
-                        nota.getDescripcion(),
-                        nota.getFecha(),
-                        nota.getEstado()
-                );
-            }
-
-
-            @Override
-            public ViewHolder_Nota onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-                View view = LayoutInflater.from(parent.getContext()).inflater(R.layout.item_nota,parent, false);
-                ViewHolder_Nota viewHolder_nota = new ViewHolder_Nota(view);
-                viewHolder_nota.setOnClickListener(new ViewHolder_Nota.ClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        Toast.makeText(Listar_Notas.this, "on item click", Toast.LENGTH_SHORT).show();
-
-                    }
-
-                    @Override
-                    public void onItemLongClick(View view, int position) {
-                        //Toast.makeText(Listar_Notas.this, "on item long click", Toast.LENGTH_SHORT).show();
-                        //Declarar las vistas
-                        Button CD_Eliminar, CD_Actualizar;
-
-                        //Realizar la conexion con el diseño
-                        CD_Eliminar = dialog.findViewById(R.id.CD_Eliminar);
-                        CD_Actualizar = dialog.findViewById(R.id.CD_Actualizar);
-
-                        CD_Eliminar.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Toast.makeText(Listar_Notas.this, "Nota eliminada", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                        CD_Actualizar.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Toast.makeText(Listar_Notas.this, "Actualizar nota", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                        dialog.show();
-                    }
-                });
-                return viewHolder_nota;
-            }
-        };
-
-        linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL, false);
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
-
-        recyclerviewNotas.setLayoutManager(linearLayoutManager);
-        recyclerviewNotas.setAdapter(firebaseRecyclerAdapter);
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        if (firebaseRecyclerAdapter!=null){
-            firebaseRecyclerAdapter.starListening();
-        }
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return super.onSupportNavigateUp();
-    }
-    }
+}
--- app/src/main/java/com/MenuPrincipal.java
+++ app/src/main/java/com/MenuPrincipal.java
@@ -1,15 +1,21 @@
 package com;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.vg.agenda_online.R;
 
 public class MenuPrincipal extends AppCompatActivity {
@@ -17,6 +23,7 @@ public class MenuPrincipal extends AppCompatActivity {
     Button CerrarSesion;
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
+    TextView NombrePrincipal, CorreoPrincipal;
 
 
     @Override
@@ -24,9 +31,12 @@ public class MenuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-        CerrarSesion=findViewById(R.id.Cerrarsesion);
+        CerrarSesion=findViewById(R.id.btnsalir);
         firebaseAuth=FirebaseAuth.getInstance();
         user=firebaseAuth.getCurrentUser();
+        //
+        NombrePrincipal=findViewById(R.id.NombrePrincipal);
+        CorreoPrincipal=findViewById(R.id.CorreoPrincipal);
 
         CerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +45,35 @@ public class MenuPrincipal extends AppCompatActivity {
             }
         });
 
+    }
+
+    //Metodo para verificar si el usuario a iniciado seccion
+
+    //Metodo para recuperar datos del usuario
+    private void CargarDatos(){
+        Query query = BASE_DE_DATOS.orderByChild("correo").equalTo(firebaseAuth.getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Recorremo, los usuarios en la base de datos
+                for (DataSnapshot ds : snapshot.getChildren()){
+
+                    //Obteniendo valores
+                    String NombrePrincipal = ""+ds.child("NombrePrincipal").getValue();
+                    String CorreoPrincipal = ""+ds.child("CorreoPrincipal").getValue();
+
+                    //Seteamos los datos
+
+
+                    //Declaramos los datos
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void SalirAplicacion() {
