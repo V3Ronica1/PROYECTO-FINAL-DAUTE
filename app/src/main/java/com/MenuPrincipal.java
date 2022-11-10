3da21b66cb5b9e9707ef2bd7f37e43688e10a316
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-11-10 08:43:50 -0600

    Cambios en Clases Java

--- app/src/main/java/com/MenuPrincipal.java
+++ app/src/main/java/com/MenuPrincipal.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.vg.agenda_online.R;
@@ -24,6 +25,7 @@ public class MenuPrincipal extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
     TextView NombrePrincipal, CorreoPrincipal;
+    DatabaseReference Usuarios;
 
 
     @Override
@@ -34,6 +36,10 @@ public class MenuPrincipal extends AppCompatActivity {
         CerrarSesion=findViewById(R.id.btnsalir);
         firebaseAuth=FirebaseAuth.getInstance();
         user=firebaseAuth.getCurrentUser();
+
+        //firebaseDatabase = firebaseDatabase.getInstance();
+        //Usuarios = firebaseDatabase.getReference("Usuarios");
+
         //
         NombrePrincipal=findViewById(R.id.NombrePrincipal);
         CorreoPrincipal=findViewById(R.id.CorreoPrincipal);
@@ -51,7 +57,7 @@ public class MenuPrincipal extends AppCompatActivity {
 
     //Metodo para recuperar datos del usuario
     private void CargarDatos(){
-        Query query = BASE_DE_DATOS.orderByChild("correo").equalTo(firebaseAuth.getUid());
+        Query query = Usuarios.orderByChild("correo").equalTo(firebaseAuth.getUid());
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
